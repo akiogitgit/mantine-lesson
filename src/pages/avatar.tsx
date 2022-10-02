dc5f723e45e5971d43c262712f5b8ea41e7cbedc
akiogitgit <akimaron2828@icloud.com>
2022-10-02 13:56:56 +0900

    feat: 画像をStorageに追加, profileに追加機能

--- src/pages/avatar.tsx
+++ src/pages/avatar.tsx
@@ -11,6 +11,7 @@ import {
 import { ReplyIcon, CameraIcon } from "@heroicons/react/solid"
 import { supabase } from "../utils/supabase"
 import { Layout } from "../components/Layout"
+import Image from "next/image"
 
 const AvatarDemo = () => {
   const [isLoading, setIsLoading] = useState(false)
@@ -18,16 +19,17 @@ const AvatarDemo = () => {
 
   const uploadAvatarImg = useCallback(
     async (e: ChangeEvent<HTMLInputElement>) => {
-      console.log(e)
-      if (!e.target.files || e.target.files.length === 0) {
+      console.log("file: ", e, e.target.files)
+      if (!e.target.files || !e.target.files.length) {
         throw new Error("Please select the image file")
       }
       const file = e.target.files[0]
       const fileExt = file.name.split(".").pop()
       const fileName = `${Math.random()}.${fileExt}`
       setIsLoading(true)
+
       const { error } = await supabase.storage
-        .from("avatars")
+        .from("avatars") // storageのavatarsにfileをupload
         .upload(fileName, file)
       if (error) {
         throw new Error(error.message)
@@ -40,6 +42,9 @@ const AvatarDemo = () => {
 
   const upsertProfile = useCallback(async () => {
     setIsLoading(true)
+
+    // DBのprofilesテーブルに、id,avatar_urlを指定して追加
+    // profilesに自分のidとavatar_urlを追加
     const { error } = await supabase.from("profiles").upsert(
       {
         id: supabase.auth.user()?.id,
@@ -51,16 +56,55 @@ const AvatarDemo = () => {
     )
     if (error) {
       throw new Error(error.message)
-      setIsLoading(false)
     }
+    setIsLoading(false)
   }, [avatarUrl])
+
+  const getProfile = useCallback(async () => {
+    const { data, error, status } = await supabase
+      .from("profiles")
+      .select("avatar_url") // profilesテーブルのavatar_urlカラム
+      .eq("id", supabase.auth.user()?.id) // ログインしているuser.idと同じ
+      .single() // １つ
+
+    // profile無いなら、406でエラーを投げない
+    if (error && status !== 400) {
+      throw new Error(error.message)
+    }
+    if (data) {
+      setAvatarUrl(data.avatar_url)
+      console.log("avatar: ", data.avatar_url)
+    }
+  }, [])
+
+  // useEffect(() => {
+  getProfile()
+  // }, [])
 
   return (
     <Layout>
       <div>Avatar</div>
-      <input type='file' name='' id='' onChange={uploadAvatarImg} />
+      {isLoading && <Loader />}
+      <label htmlFor='avatar'>avatar</label>
+      <input
+        type='file'
+        id='avatar'
+        accept='image/*'
+        className='cursor-pointer file:text-white file:rounded-full file:bg-purple-400 file:border-0 file:py-2 file:px-4'
+        onChange={uploadAvatarImg}
+      />
+      <Button onClick={upsertProfile}>Upsert</Button>
+      <Button color='cyan' onClick={getProfile}>
+        getAvatar
+      </Button>
+      {avatarUrl && (
+        <>
+          <div>{avatarUrl}</div>
+          <Image src={avatarUrl} width={50} height={50} alt='' />
+        </>
+      )}
       {/* <FileInput multiple onChange={e => uploadAvatarImg(e)} /> */}
-      <FileInput multiple onChange={e => console.log(e)} />
+      {/* <FileInput multiple onChange={e => console.log(e)} /> */}
     </Layout>
   )
 }
