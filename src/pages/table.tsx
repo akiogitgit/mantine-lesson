7ca591746034aa4cdcb2a76016451654cace5b12
akiogitgit <akimaron2828@icloud.com>
2022-10-16 23:48:49 +0900

    feat: Tableのオプション追加

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,19 +1,15 @@
 import React from "react"
-import { Table } from "@mantine/core"
-import { Settings } from "tabler-icons-react"
+import { Settings, Space } from "tabler-icons-react"
 import { IconSettings, IconPacman, IconToolsKitchen2 } from "@tabler/icons"
 import { Layout } from "../components/Layout"
 import { NextPage } from "next"
+import { useGetApi } from "../hooks/useGetApi"
+import { Todo } from "../types/todo"
+import { Table } from "@mantine/core"
 
 const TableDemo: NextPage = () => {
-  // const rows = elements.map(element => (
-  //   <tr key={element.name}>
-  //     <td>{element.position}</td>
-  //     <td>{element.name}</td>
-  //     <td>{element.symbol}</td>
-  //     <td>{element.mass}</td>
-  //   </tr>
-  // ))
+  const { data: todos } = useGetApi<Todo>("todos", {})
+  console.log(todos)
 
   return (
     <Layout>
@@ -22,7 +18,7 @@ const TableDemo: NextPage = () => {
       <IconSettings />
       <IconPacman />
       <IconToolsKitchen2 />
-      <iframe
+      {/* <iframe
         id='player'
         width='300'
         height='200'
@@ -30,7 +26,37 @@ const TableDemo: NextPage = () => {
         src={"https://www.youtube.com/embed/" + "auLih2TFwio"}
         frameBorder='0'
         allowFullScreen
-      />
+      /> */}
+
+      <Table
+        horizontalSpacing='xl'
+        verticalSpacing='md'
+        striped
+        highlightOnHover
+        withBorder
+        withColumnBorders
+        className='mx-auwithColumnBorderso max-w-600px'
+        my='xl'
+      >
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>created_at</th>
+          </tr>
+        </thead>
+        {todos && (
+          <tbody>
+            {todos.map(todo => (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+                <td>{todo.created_at}</td>
+              </tr>
+            ))}
+          </tbody>
+        )}
+      </Table>
 
       <Table className='mx-auto max-w-600px'>
         <thead>
