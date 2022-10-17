f24899db0db5a180406589fb6a02f487714785d4
akiogitgit <akimaron2828@icloud.com>
2022-10-18 08:57:58 +0900

    feat: useInterval

--- src/pages/hooks.tsx
+++ src/pages/hooks.tsx
@@ -1,8 +1,17 @@
 import { Layout } from "../components/Layout"
-import { BrandGithub } from "tabler-icons-react"
-import { useDisclosure, useToggle } from "@mantine/hooks"
-import { Button, Dialog, Group, Stack, Text, TextInput } from "@mantine/core"
+import { Blockquote, BrandGithub } from "tabler-icons-react"
+import { useDisclosure, useInterval, useToggle } from "@mantine/hooks"
+import {
+  Button,
+  Dialog,
+  Group,
+  Kbd,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core"
 import { showNotification } from "@mantine/notifications"
+import { useCallback, useEffect, useState } from "react"
 
 const Hooks = () => {
   const [opened, setOpened] = useDisclosure(false, {
@@ -10,6 +19,20 @@ const Hooks = () => {
     onClose: () => console.log("close"),
   })
   const [buttonColor, toggleButtonColor] = useToggle(["teal", "pink"])
+
+  const [seconds, setSeconds] = useState(0)
+  const interval = useInterval(() => setSeconds(s => s + 1), 100)
+  useEffect(() => {
+    interval.start()
+    return interval.stop
+  }, [])
+
+  const handleFizzBuzz = useCallback((num: number): number | string => {
+    if (num % 15 === 0) return "FizzBuzz"
+    if (num % 3 === 0) return "Fizz"
+    if (num % 5 === 0) return "Buzz"
+    return num
+  }, [])
 
   return (
     <Layout>
@@ -24,7 +47,16 @@ const Hooks = () => {
         <Button color={buttonColor} onClick={() => toggleButtonColor()}>
           Toggle Color
         </Button>
+        <Kbd>{handleFizzBuzz(seconds)}</Kbd>
+        <Button
+          onClick={interval.toggle}
+          color={interval.active ? "red" : "teal"}
+          variant='outline'
+        >
+          {interval.active ? "Stop" : "Start"} counting
+        </Button>
       </Stack>
+
       <Dialog
         opened={opened}
         withCloseButton
