a92fc0314e2a72e424374281a13b2de2749f770c
akiogitgit <akimaron2828@icloud.com>
2022-10-17 10:30:54 +0900

    feat: レイアウト作成

--- src/pages/crypto.tsx
+++ src/pages/crypto.tsx
@@ -1,3 +1,5 @@
+import { LightningBoltIcon } from "@heroicons/react/solid"
+import { Text, Loader, Stack } from "@mantine/core"
 import { DatePicker } from "@mantine/dates"
 import axios from "axios"
 import { getDate, getMonth, getYear } from "date-fns"
@@ -14,6 +16,7 @@ const Crypto = () => {
   const [date, setDate] = useState<Date | null>(new Date())
   const [btc, setBtc] = useState<Coin | null>(null)
   const [eth, setEth] = useState<Coin | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const getCoinHistory = useCallback(
     async (coinId: string, dateParam: string) => {
@@ -44,33 +47,48 @@ const Crypto = () => {
     [],
   )
 
-  const setCoinHistory = useCallback(async () => {
+  useEffect(() => {
     // coingeckoのparamsに合わせる
     const dateParam = date
       ? `date=${getDate(date!)}-${getMonth(date!) + 1}-${getYear(date!)}`
       : ""
-
-    // coingecko で仮想通貨のレートを取得
-    if (dateParam) {
-      setBtc(await getCoinHistory("bitcoin", dateParam))
-      setEth(await getCoinHistory("ethereum", dateParam))
+    const setCoinHistory = async () => {
+      // coingecko で仮想通貨のレートを取得
+      if (dateParam) {
+        setIsLoading(true)
+        setBtc(await getCoinHistory("bitcoin", dateParam))
+        setEth(await getCoinHistory("ethereum", dateParam))
+        setIsLoading(false)
+      }
     }
-  }, [date, getCoinHistory])
-
-  useEffect(() => {
     setCoinHistory()
-  }, [setCoinHistory])
+  }, [date, getCoinHistory])
 
   return (
     <Layout>
-      {/* {date && JSON.stringify(date)} */}
-      <DatePicker
-        placeholder='Pick date'
-        label='Event date'
-        withAsterisk
-        onChange={setDate}
-      />
-      <p>{btc && btc.market_data?.current_price?.jpy}</p>
+      <Stack justify='center' align='center'>
+        <LightningBoltIcon className='h-10 mb-4 text-blue-500 w-10' />
+        {isLoading && <Loader size='xl' variant='bars' />}
+        <Text mt='md'>
+          <Text color='blue' component='span'>
+            Bitcoin
+          </Text>{" "}
+          {Math.round(Number(btc?.market_data?.current_price.jpy) * 100) / 100}
+        </Text>
+        <Text mt='md'>
+          <Text color='cyan' component='span'>
+            Ethereum
+          </Text>{" "}
+          {Math.round(Number(eth?.market_data?.current_price.jpy) * 100) / 100}
+        </Text>
+        <DatePicker
+          mt='xl'
+          placeholder='Pick date'
+          label='Event date'
+          withAsterisk
+          onChange={setDate}
+        />
+      </Stack>
     </Layout>
   )
 }
